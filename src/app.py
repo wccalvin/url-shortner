c26b6d4b4524c78546b4aa85d8778306621018f3
wccalvin <clayton.calvin@gmail.com>
2021-07-23 07:27:02 -0400

    write form values to json

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
@@ -11,6 +13,11 @@ def index():
 @app.route("/given-url", methods=["GET", "POST"])
 def given_url():
     if request.method == "POST":
+        urls = {}
+        short_name = request.form["code"]
+        urls[short_name] = {"url": request.form["url"]}
+        with open("urls.json", "w") as url_file:
+            json.dump(urls, url_file)
         return render_template("given_url.html", code=request.form["code"])
     return redirect(url_for("index"))
 
